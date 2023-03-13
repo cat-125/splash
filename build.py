fee570d00d3683430ee3c915d5dee1d5bb8de3fb
sergeas1805@gmail.com <sergeas1805@gmail.com>
2023-03-13 11:18:22 +0300

    build files

--- build.py
+++ build.py
@@ -1,1 +1,26 @@
-print('Test')
+import os, re
+
+IMPORT_REGEXP = re.compile(r"@import url\(\'(.+)\'\);")
+
+def read(path):
+	file = open(path, "r")
+	text = file.read()
+	file.close()
+	return text
+
+def write(path, text):
+		file = open(path, "w")
+		file.write(text)
+		file.close()
+
+def process(path):
+	text = read(path)
+	os.chdir(path)
+	text = re.sub(IMPORT_REGEXP, lambda x: read(x))
+	return text
+	
+def build(src, result):
+	write(result, process(src))
+
+if __name__ == "__main__":
+	build("./css/main.css", "./build.css")
