1bfd31b6a4cb622b862584a4a7bef8ec3fedff5a
sergeas1805@gmail.com <sergeas1805@gmail.com>
2023-05-21 20:28:59 +0300

    Update build.py

--- build.py
+++ build.py
@@ -1,5 +1,8 @@
-import os, re
+import os, re, time
 from os.path import abspath
+import sys
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler, EVENT_TYPE_OPENED
 
 IMPORT_REGEXP = re.compile(r"@import url\(\'(.+)\'\);")
 MINIMIZE_REGEXP = re.compile(r"(\n\s*)|(/\*(.|\n)+?\*/)")
@@ -13,14 +16,14 @@ def read(path):
 	return text
 
 def write(path, text):
-	print('WRITE: ' + path)
+	# print('WRITE: ' + path)
 	file = open(path, "w")
 	file.write(text)
 	file.close()
 
 def process(path):
 	os.chdir(HOME_DIR + '/src')
-	print('PROCESS: ' + path)
+	# print('PROCESS: ' + path)
 	filedir = '/'.join( path.split('/')[0:-1] );
 	text = read(path)
 	os.chdir( filedir )
@@ -34,8 +37,8 @@ def build(src, result):
 	os.chdir(HOME_DIR)
 	write(result, processed)
 
-def minimize(src, result):
-	print('MINIMIZE: ' + src)
+def minify(src, result):
+	print('MINIFY: ' + src)
 	text = read(src)
 	text = re.sub(MINIMIZE_REGEXP, '', text)
 	text = re.sub(MINIMIZE_REGEXP2, lambda x: x.group(1) or x.group(2), text)
@@ -43,4 +46,20 @@ def minimize(src, result):
 
 if __name__ == "__main__":
 	build("./main.css", "./build.css")
-	minimize('./build.css', './build.min.css')
+	minify('./build.css', './build.min.css')
+	class BuildHandler(FileSystemEventHandler):
+		def on_any_event(self, event):
+			if event.event_type == EVENT_TYPE_OPENED:
+				return
+			print(f'Detected changes in {event.src_path}, rebuilding...')
+			build("./main.css", "./build.css")
+			minify('./build.css', './build.min.css')
+	observer = Observer()
+	observer.schedule(BuildHandler(), 'src')
+	observer.start()
+	try:
+		while True:
+			time.sleep(1)
+	except KeyboardInterrupt:
+		observer.stop()
+	observer.join()
